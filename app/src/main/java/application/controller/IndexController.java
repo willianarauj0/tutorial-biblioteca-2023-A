81a9bf87c4c4a71fac211372ccc6a48878dfd70f
marcoweb <marcocarvalho.web@gmail.com>
2023-10-31 19:56:45 +0000

    Genero

--- app/src/main/java/application/controller/IndexController.java
+++ app/src/main/java/application/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     public String home() {
-        return "WEB-INF/home.jsp";
+        return "home";
     }
 }
 
