dfa20515a9fad7962964a9ea7862300347030a72
marcoweb <marcocarvalho.web@gmail.com>
2023-11-07 18:41:54 +0000

    Livro - Insert

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.GeneroRepository;
+import application.model.Livro;
 import application.model.LivroRepository;
 
 @Controller
@@ -27,4 +30,20 @@ public class LivroController {
         model.addAttribute("generos", generoRepo.findAll());
         return "livro/insert";
     }
-}
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(
+        @RequestParam("titulo") String titulo,
+        @RequestParam("isbn") String isbn,
+        @RequestParam("genero") int idGenero
+    ) {
+        Livro livro = new Livro();
+        livro.setTitulo(titulo);
+        livro.setIsbn(isbn);
+        livro.setGenero(generoRepo.findById(idGenero).get());
+
+        livroRepo.save(livro);
+
+        return "redirect:/livro/list";
+    }
+ }
